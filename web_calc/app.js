cc8350e1f76b72e88e4e5f596a8f825728cf0527
Carlos Salcán <cdsalcan131@gmail.com>
2024-08-26 22:23:28 -0500

    Prueba de eventos click para botones en la calculadora
    - Configuración inicial de los botones en la calculadora.
    - Implementación de eventos de clic para los botones "1" y "5".
    - Prueba en consola para verificar que los botones muestren el valor correcto al ser clicados.

---
+++ web_calc/app.js
@@ -0,0 +1,12 @@
+//-------------------------------> SELECCIONAR BTN
+const button1 = document.getElementById('btn-1');
+const button5 = document.getElementById('btn-5');
+
+//-------------------------------> EVENTO CLICK
+button1.addEventListener('click', function() {
+    console.log('1');
+});
+
+button5.addEventListener('click', function() {
+    console.log('5');
+});
