5907fc2c39675e3da05e3f73531bec8cf1abed29
Carlos Salcán <cdsalcan131@gmail.com>
2024-08-28 22:12:45 -0500

    Añadida funcionalidad actualizarDisplay con botones
    - Implementación de la función `actualizarDisplay` para manejar la actualización del display de la calculadora al hacer clic en los botones numéricos y de operación.
    - Asignación de eventos de clic a todos los botones de la calculadora.
    - Modificación del `main` en el HTML para integrar correctamente el display y los botones de la calculadora.

--- web_calc/app.js
+++ web_calc/app.js
@@ -1,12 +1,47 @@
 //-------------------------------> SELECCIONAR BTN
-const button1 = document.getElementById('btn-1');
-const button5 = document.getElementById('btn-5');
+const buttons = document.querySelectorAll('.btn');
 
+//-------------------------------> SELECCIONAR INPUT
+const resultInput = document.getElementById('screen');
+
+/*
 //-------------------------------> EVENTO CLICK
-button1.addEventListener('click', function() {
+button1.addEventListener('click', function () {
     console.log('1');
-});
+});*/
 
-button5.addEventListener('click', function() {
-    console.log('5');
+/**
+ *  Funcion para actualizar el valor del display de la calculadora.
+ * 
+ *  @param {Event} event - El evento de clic que se dispara al presionar un botón.
+ *  @param {HTMLInputElement} resultInput - El campo de entrada que actúa como display de la calculadora.
+ * 
+ *  
+ * @description
+ *  Esta función se encarga de modificar el valor del campo de entrada (`input`) que actúa como 
+ *  pantalla de la calculadora. Si el valor actual en el display es "0", este valor será reemplazado 
+ *  por el valor del botón clicado. En caso contrario, el valor del botón se concatena al valor 
+ *  existente en el display.
+ * 
+ *  @example
+ *  Si el display muestra "0" y se hace clic en el botón "5", el display mostrará "5".
+ *  Si el display muestra "12" y se hace clic en el botón "3", el display mostrará "123".
+ * 
+ *  @returns {void} - No retorna ningún valor.
+ */
+function actualizarDisplay(event) {
+    // GET value btn click
+    const buttonValue = event.target.textContent;
+
+    //Reemplazar "0" por el valor del botón clicado, de lo contrario, concatenar
+    if (resultInput.value === "0") {
+        resultInput.value = buttonValue;
+    } else {
+        resultInput.value += buttonValue;
+    }
+}
+
+//-------------------------------> ASIGNAR A BTNS
+buttons.forEach(button => {
+    button.addEventListener('click', actualizarDisplay);
 });
